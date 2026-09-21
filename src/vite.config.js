2efad90fbeb84c9d9acba36494b997acf8b6db08
8windwind8 <835258908@qq.com>
2026-09-21 21:10:56 +0800

    Add files via upload

---
+++ src/vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    
+  ],
+  server: {                    // ← 跨域配置写这里
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1236',   // 你的后端地址
+        changeOrigin: true,
+       
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
